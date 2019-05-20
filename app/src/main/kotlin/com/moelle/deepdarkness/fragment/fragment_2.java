d0593ecedd5ee0810c49f2c6c51e11ee0bfe8336
moelle89 <manuel.moellmann@gmail.com>
2019-05-20 03:08:13 +0200

    ...

--- app/src/main/kotlin/com/moelle/deepdarkness/fragment/fragment_2.java
+++ app/src/main/kotlin/com/moelle/deepdarkness/fragment/fragment_2.java
@@ -19,8 +19,6 @@ import com.moelle.deepdarkness.DirectoryHelper;
 import com.moelle.deepdarkness.DownloadService;
 import com.moelle.deepdarkness.R;
 
-import static android.widget.Toast.LENGTH_SHORT;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,7 +26,27 @@ import static android.widget.Toast.LENGTH_SHORT;
 public class fragment_2 extends Fragment implements View.OnClickListener{
 
     // image url to download
-    private static final String accent1 = "https://3.bp.blogspot.com/-EFwVj5ztKtQ/V8Qs6Viyl6I/AAAAAAAADWs/031SPYFrUnM-wreztTT4fgPe1yQj3LFhgCPcB/s1600/developer.jpg";
+    private static final String accent1 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/1.png";
+    private static final String accent2 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/2.png";
+    private static final String accent3 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/3.png";
+    private static final String accent4 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/4.png";
+    private static final String accent5 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/5.png";
+    private static final String accent6 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/6.png";
+    private static final String accent7 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/7.png";
+    private static final String accent8 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/8.png";
+    private static final String accent9 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/9.png";
+    private static final String accent10 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/10.png";
+    private static final String accent11 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/11.png";
+    private static final String accent12 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/12.png";
+    private static final String accent13 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/13.png";
+    private static final String accent14 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/14.png";
+    private static final String accent15 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/15.png";
+    private static final String accent16 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/16.png";
+    private static final String accent17 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/17.png";
+    private static final String accent18 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/18.png";
+    private static final String accent19 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/19.png";
+    private static final String accent20 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/20.png";
+
     private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 54654;
 
 
@@ -108,97 +126,97 @@ public class fragment_2 extends Fragment implements View.OnClickListener{
                 break;
             }
             case R.id.accent2: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent2, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent3: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent3, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent4: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent4, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent5: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent5, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent6: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent6, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent7: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent7, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent8: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent8, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent9: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent9, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent10: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent10, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent11: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent11, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent12: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent12, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent13: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent13, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent14: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent14, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent15: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent15, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent16: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent16, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent17: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent17, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent18: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent18, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent19: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent19, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
             case R.id.accent20: {
-                getActivity().startService(DownloadService.getDownloadService(getContext(), accent1, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
+                getActivity().startService(DownloadService.getDownloadService(getContext(), accent20, DirectoryHelper.ROOT_DIRECTORY_NAME.concat("/")));
                 Toast();
                 break;
             }
@@ -213,6 +231,6 @@ public class fragment_2 extends Fragment implements View.OnClickListener{
         }
     }
     public void Toast(){
-        Toast.makeText(getActivity(), "DOWNLOAD SUCCESSFUL", LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "DOWNLOAD SUCCESSFUL", Toast.LENGTH_LONG).show();
     }
 }
