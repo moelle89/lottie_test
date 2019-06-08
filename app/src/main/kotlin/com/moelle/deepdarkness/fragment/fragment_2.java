01c62e48224bc17536f770d132958bd5d157f617
moelle89 <manuel.moellmann@gmail.com>
2019-06-08 09:11:10 +0200

    ...

--- app/src/main/kotlin/com/moelle/deepdarkness/fragment/fragment_2.java
+++ app/src/main/kotlin/com/moelle/deepdarkness/fragment/fragment_2.java
@@ -1,16 +1,26 @@
 package com.moelle.deepdarkness.fragment;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.ColorFilter;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -46,6 +56,9 @@ public class fragment_2 extends Fragment implements View.OnClickListener {
     private static final int DIALOG_ID1 = 0;
     private static final int DIALOG_ID2 = 1;
 
+    private LottieAnimationView keyboard2;
+    private ImageView closeBG, iconTG, iconMAIL, imageView;
+    private CardView tg, mail;
     private TextView cat_top1, cat_top2, cat_bottom;
     // image url to download
     private static final String accent6 = "https://raw.githubusercontent.com/moelle89/deepdarkness/master/Accents/6.png";
@@ -150,6 +163,7 @@ public class fragment_2 extends Fragment implements View.OnClickListener {
 
         card1.setOnClickListener(this);
         card2.setOnClickListener(this);
+        CardView4.setOnClickListener(this);
         card2.setAlpha(0f);
         card2.animate().alpha(1f).setDuration(600).setStartDelay(100);
         card1.setAnimation(vonOben);
@@ -173,6 +187,10 @@ public class fragment_2 extends Fragment implements View.OnClickListener {
             }
             case R.id.accent4: {
                 launchPicker2(getView());
+                break;
+            }
+            case R.id.CardView4: {
+                showContact();
                 break;
             }
         }
@@ -206,5 +224,127 @@ public class fragment_2 extends Fragment implements View.OnClickListener {
                 .setAllowCustom(true)
                 .setShowAlphaSlider(false)
                 .show(getActivity());
+    }
+
+    private void showContact() {
+        final View dialogView = View.inflate(getActivity(), R.layout.dialog_gboard, null);
+        final Dialog dialog = new Dialog(getActivity(), R.style.ApptThemeDialogContact);
+        dialog.setContentView(dialogView);
+
+        keyboard2 = dialog.findViewById(R.id.dashboard_head2);
+
+        keyboard2.setBackgroundTintList(ColorStateList.valueOf(pickedColor1));
+        SimpleColorFilter keyboardtint = new SimpleColorFilter(pickedColor1);
+        KeyPath keyfg3 = new KeyPath("BG2","**");
+        LottieValueCallback<ColorFilter> callback3 = new LottieValueCallback<ColorFilter>(keyboardtint);
+        keyboard2.addValueCallback(keyfg3, LottieProperty.COLOR_FILTER, callback3);
+
+        final int fg = ContextCompat.getColor(dialog.getContext(), R.color.textColor);
+        SimpleColorFilter filterfg = new SimpleColorFilter(fg);
+        KeyPath keyboardKey = new KeyPath("fg", "**");
+        LottieValueCallback<ColorFilter> keyboardCall = new LottieValueCallback<ColorFilter>(filterfg);
+        keyboard2.addValueCallback(keyboardKey, LottieProperty.COLOR_FILTER, keyboardCall);
+
+
+        keyboard2.setBackgroundTintList(ColorStateList.valueOf(pickedColor2));
+        SimpleColorFilter keyboardtint2 = new SimpleColorFilter(pickedColor2);
+        KeyPath keyfg4 = new KeyPath("BG1","**");
+        LottieValueCallback<ColorFilter> callback4 = new LottieValueCallback<ColorFilter>(keyboardtint2);
+        keyboard2.addValueCallback(keyfg4, LottieProperty.COLOR_FILTER, callback4);
+
+
+        imageView = dialog.findViewById(R.id.closeDialogImg);
+        iconMAIL = dialog.findViewById(R.id.iconMAIL);
+        mail = dialog.findViewById(R.id.mail);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                revealShow(dialogView, false, dialog);
+            }
+        });
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                revealShow(dialogView, true, null);
+            }
+        });
+        dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialogInterface, int i, KeyEvent keyEvent) {
+                if (i == KeyEvent.KEYCODE_BACK) {
+
+                    revealShow(dialogView, false, dialog);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.show();
+
+        mail.setAlpha(0.0f);
+        mail.setScaleX(0.7f);
+        mail.setScaleY(0.7f);
+        mail.setTranslationY(200);
+        mail.animate().translationY(0).scaleX(1).scaleY(1).alpha(1f).setStartDelay(550).setDuration(650).setInterpolator(new FastOutSlowInInterpolator()).start();
+        iconMAIL.setAlpha(0.0f);
+        iconMAIL.setScaleX(0.8f);
+        iconMAIL.setScaleY(0.8f);
+        iconMAIL.setTranslationY(250);
+        iconMAIL.animate().translationY(0).scaleX(1).scaleY(1).alpha(1f).setStartDelay(750).setDuration(650).setInterpolator(new FastOutSlowInInterpolator()).start();
+        keyboard2.setAlpha(0.0f);
+        keyboard2.setTranslationX(200);
+        keyboard2.animate().translationX(0).alpha(1f).setStartDelay(0).setDuration(750).setInterpolator(new AccelerateInterpolator()).start();
+        imageView.setAlpha(0.0f);
+        imageView.setScaleX(0.5f);
+        imageView.setScaleY(0.5f);
+        imageView.setRotation(120);
+        imageView.animate().rotation(0).scaleX(1).scaleY(1).alpha(0.8f).setStartDelay(550).setDuration(750).setInterpolator(new FastOutSlowInInterpolator()).start();
+
+    }
+    private void revealShow(View dialogView, boolean b, final Dialog dialog) {
+
+        final View view = dialogView.findViewById(R.id.dialog_gboard);
+
+        int w = view.getWidth();
+        int h = view.getHeight();
+
+        int endRadius = (int) Math.hypot(w, h);
+
+        int cx = (int) (cat_top1.getX() + (cat_top1.getWidth() / 2));
+        int cy = (int) (cat_top1.getY()) + cat_top1.getHeight() - 90;
+
+
+        if (b) {
+            Animator revealAnimator = ViewAnimationUtils.createCircularReveal(view, cx, cy, 80, endRadius);
+
+            view.setVisibility(View.VISIBLE);
+            revealAnimator.setDuration(800);
+            revealAnimator.start();
+
+
+        } else {
+            Animator anim =
+                    ViewAnimationUtils.createCircularReveal(view, cx, cy, endRadius, 0);
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    view.setVisibility(View.INVISIBLE);
+                    dialog.dismiss();
+                }
+            });
+            mail.animate().translationY(200).scaleX(0.8f).scaleY(0.8f).alpha(0f).setStartDelay(80).setDuration(600).setInterpolator(new FastOutSlowInInterpolator()).start();
+            iconMAIL.animate().translationY(200).scaleX(0.7f).scaleY(0.7f).alpha(0f).setStartDelay(180).setDuration(500).setInterpolator(new FastOutSlowInInterpolator()).start();
+            keyboard2.animate().translationX(200).alpha(0f).setStartDelay(0).setDuration(600).setInterpolator(new AccelerateInterpolator()).start();
+            imageView.animate().rotation(120).scaleX(0.5f).scaleY(0.5f).alpha(0f).setStartDelay(0).setDuration(600).setInterpolator(new FastOutSlowInInterpolator()).start();
+            anim.setDuration(750);
+            anim.setStartDelay(100);
+            anim.start();
+
+        }
     }
 }
