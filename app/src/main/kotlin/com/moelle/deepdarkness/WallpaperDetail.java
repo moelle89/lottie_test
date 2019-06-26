57d7a51dbf4692d73b56ad71f612709ab8b6bd3d
Anas Karbila <anaskarbila@aospa.co>
2019-06-26 17:40:26 +0200

    Implement wallpaper applying

--- app/src/main/kotlin/com/moelle/deepdarkness/WallpaperDetail.java
+++ app/src/main/kotlin/com/moelle/deepdarkness/WallpaperDetail.java
@@ -1,7 +1,13 @@
 package com.moelle.deepdarkness;
 
 import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -19,6 +25,8 @@ import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.ExecutionException;
 
 public class WallpaperDetail extends AppCompatActivity {
 
@@ -41,11 +49,12 @@ public class WallpaperDetail extends AppCompatActivity {
 
     void iniViews() {
         play_fab = findViewById(R.id.play_fab);
-        String imgURL = getIntent().getExtras().getString("imgURL");
+        final String imgURL = getIntent().getExtras().getString("imgURL");
         String imgCover = getIntent().getExtras().getString("imgCover");
         MovieThumbnailImg = findViewById(R.id.detail_wall_img);
-        //Glide.with(this).load(imageResourceId).into(MovieThumbnailImg);
-        Glide.with(this).load(imgURL)
+
+        Glide.with(this)
+                .load(imgURL)
                 .apply(RequestOptions.bitmapTransform(new RoundedCorners(14)))
                 .into(MovieThumbnailImg);
         MovieCoverImg = findViewById(R.id.detail_wall_cover);
@@ -60,20 +69,76 @@ public class WallpaperDetail extends AppCompatActivity {
         MovieCoverImg.setAnimation(anim_cover);
         play_fab.setAnimation(anim_fab);
         //MovieThumbnailImg.setAnimation(anim_thumb);
+
         play_fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                // TODO Auto-generated method stub
-                WallpaperManager myWallpaperManager
-                        = WallpaperManager.getInstance(getApplicationContext());
-                try {
-                    myWallpaperManager.setResource(R.raw.w7);
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+                new LoadBitmapTask(getApplicationContext(), imgURL).execute();
             }
         });
     }
 
+    static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        int w = drawable.getIntrinsicWidth();
+        int h = drawable.getIntrinsicHeight();
+
+        if (w <= 0 || h <= 0) {
+            return null;
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    static class LoadBitmapTask extends AsyncTask<Void, Void, Drawable> {
+        private WeakReference<Context> contextWeakReference;
+        private String imgUrl;
+
+        LoadBitmapTask(Context context, String imgUrl) {
+            contextWeakReference = new WeakReference<>(context);
+            this.imgUrl = imgUrl;
+        }
+
+        @Override
+        protected Drawable doInBackground(Void... params) {
+            Context context = contextWeakReference.get();
+            if (context != null) {
+                try {
+                    return Glide.with(context)
+                            .load(imgUrl)
+                            .submit()
+                            .get();
+                } catch (ExecutionException | InterruptedException ignored) {
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Drawable wallpaper) {
+            Context context = contextWeakReference.get();
+            if (wallpaper != null && context != null) {
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
+                try {
+                    Bitmap bitmap = drawableToBitmap(wallpaper);
+                    if (bitmap != null) {
+                        wallpaperManager.setBitmap(bitmap);
+                    }
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
 }
